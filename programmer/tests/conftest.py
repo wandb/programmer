776e09486e0c0a2be79a51175d3333c485556c64
Shawn Lewis <shlewis@gmail.com>
2024-08-23 06:44:14 -0700

    Filter all deprecation warnings in pytest

--- programmer/tests/conftest.py
+++ programmer/tests/conftest.py
@@ -3,29 +3,6 @@ import warnings
 import pytest
 import logging
 
-# Disable the specific DeprecationWarning for distutils
-warnings.filterwarnings(
-    "ignore",
-    category=DeprecationWarning,
-    message="The distutils package is deprecated and slated for removal",
-)
-
-# Disable SentryHubDeprecationWarning
-warnings.filterwarnings(
-    "ignore",
-    category=DeprecationWarning,  # or use SentryHubDeprecationWarning if it's a custom category
-    message="`sentry_sdk.Hub` is deprecated and will be removed",
-)
-
-# Disable Jupyter platformdirs warning
-warnings.filterwarnings(
-    "ignore",
-    category=DeprecationWarning,
-    message="Jupyter is migrating its paths to use standard platformdirs",
-)
-
-# Must filter warnings above weave imports to prevent them logging in tests
-
 from weave.weave_client import WeaveClient
 from weave.trace_server.sqlite_trace_server import SqliteTraceServer
 from weave.weave_init import InitializedClient
